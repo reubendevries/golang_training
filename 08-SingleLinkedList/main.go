991a4a9a65be4b338d8a36ef571c398280fa9abd
Reuben deVries <reubendevries@gmail.com>
2023-11-20 20:03:36 -0800

    moved conditionals to lesson 05 moved pointers to lesson 06 move channels to lesson 07 added singlelinkedlist as lesson 08
    Signed-off-by: Reuben deVries <reubendevries@gmail.com>

---
+++ 08-SingleLinkedList/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type Node struct {
+	info interface{}
+	next *Node
+}
+
+type List struct {
+	head *Node
+}
+
+func (l *List) Insert(d interface{}) {
+	list := &Node{info: d, next: nil}
+	if l.head == nil {
+		l.head = list
+	} else {
+		p := l.head
+		for p.next != nil {
+			p = p.next
+		}
+		p.next = list
+	}
+}
+
+func Show(l *List) {
+	p := l.head
+	for p != nil {
+		fmt.Printf("-> %v ",p.info)
+		p = p.next
+	}
+}
+
+func main() {
+	sl := List{}
+	for i := 0; i < 5; i++ {
+		sl.Insert(rand.Intn(100))
+	}
+	Show(&sl)
+}
