9f85ab0f8b0960696d0280a8a0528fba38fbefd9
Reuben deVries <reubendevries@gmail.com>
2023-11-21 22:53:54 -0800

    created a maps example. created a slices example.

--- 09-Maps/main.go
+++ 09-Maps/main.go
@@ -1,6 +1,21 @@
 package main
 
-import "log"
+import (
+	"log"
+	"time"
+)
+
+type User struct {
+	FirstName string
+	MiddleName string
+	LastName string
+	Age int
+	BirthDate time.Time
+}
+
+func(u *User) printFirstName() string {
+	return u.FirstName
+}
 
 func main() {
 	myMap := make(map[string]int)
@@ -8,5 +23,7 @@ func main() {
 	myMap["First"] = 1
 	myMap["Second"] = 2
 
-	log.Println(myMap["First"], myMap["Second"])
+
+	log.Println(myMap["First"])
+	log.Println(myMap["Second"])
 }
