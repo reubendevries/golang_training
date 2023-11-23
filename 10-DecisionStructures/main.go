71c9724c226249f71f1bd13313dbdd10ef3a2bca
Reuben deVries <reubendevries@gmail.com>
2023-11-22 18:56:10 -0800

    added conditionals with case.

---
+++ 10-DecisionStructures/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "log"
+
+func main() {
+	myVar := "horse"
+
+	switch myVar {
+	case "cat":
+		log.Println("cat is set to cat")
+	case "dog":
+		log.Println("cat is set to dog")
+	case "fish":
+		log.Println("cat is set to fish")
+	default:
+		log.Println("cat is set to something else")
+	}
+}
