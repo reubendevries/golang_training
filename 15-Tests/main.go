7a8d64b0b3d53bb4011b5f148b98e7df368fcd26
Reuben deVries <reubendevries@gmail.com>
2023-11-23 20:02:00 -0800

    added tests for Testing purposes.
    Signed-off-by: Reuben deVries <reubendevries@gmail.com>

--- 15-Tests/main.go
+++ 15-Tests/main.go
@@ -1,11 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"log"
+)
 
 func main() {
-	fmt.Println("vim-go")
+
+	result, err := divide(100.0, 10.0)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println("result of division is", result)
+
 }
 
-func Bar() string {
-	return "bar"
+func divide(x, y float32) (float32, error) {
+	var result float32
+
+	if y == 0 {
+		return result, errors.New("cannot divide by 0")
+	}
+
+	result = x / y
+
+	return result, nil
 }
--- 15-Tests/main_test.go
+++ 15-Tests/main_test.go
@@ -2,17 +2,32 @@ package main
 
 import "testing"
 
-func TestFoo(t *testing.T) {
-	t.Error("intentional error 1")
+var tests = []struct {
+	name     string
+	dividend float32
+	divisor  float32
+	expected float32
+	isErr    bool
+}{
+	{"valid-data", 100.0, 10.0, 10.0, false},
+	{"invalid-data", 100.0, 0.0, 0.0, true},
+	{"expected-5", 50.0, 10.0, 5.0, false},
 }
 
-func TestBar(t *testing.T) {
-	result := Bar()
-	if result != "bar" {
-		t.Errorf("expecing bar, go %s", result)
+func TestDivision(t *testing.T) {
+	for _, tt := range tests {
+		got, err := divide(tt.dividend, tt.divisor)
+		if tt.isErr {
+			if err == nil {
+				t.Error("Expected an error but didn't get one")
+			}
+		} else {
+			if err != nil {
+				t.Error("Didn't expect an error but got one", err.Error())
+			}
+		}
+		if got != tt.expected {
+			t.Errorf("expected %f but got %f", tt.expected, got)
+		}
 	}
-}
-
-func TestQuiz(t *testing.T) {
-	t.Error("intentional error 2")
 }
