91c650ea66d64f416c0617fc04092c5cb6b14e36
Reuben deVries <reubendevries@gmail.com>
2023-11-27 17:43:49 -0800

    Finished Section-02
    Signed-off-by: Reuben deVries <reubendevries@gmail.com>

---
+++ Section-02/11-SharingDataTemplates/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"golang_training/Section-02/11-SharingDataTemplates/config"
+	"golang_training/Section-02/11-SharingDataTemplates/handlers"
+	"golang_training/Section-02/11-SharingDataTemplates/render"
+	"log"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// main is the main application function.
+func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache.")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
+
+	fmt.Printf("Starting application on port %s", portNumber)
+	_ = http.ListenAndServe(portNumber, nil)
+}
