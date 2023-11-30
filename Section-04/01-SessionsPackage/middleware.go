f792e51d529c8430ac9a38c633bfd110aaffe283
Reuben deVries <reubendevries@gmail.com>
2023-11-29 19:59:36 -0800

    Finished first lesson.
    Signed-off-by: Reuben deVries <reubendevries@gmail.com>

--- Section-04/01-SessionsPackage/middleware.go
+++ Section-04/01-SessionsPackage/middleware.go
@@ -6,14 +6,23 @@ import (
 	"github.com/justinas/nosurf"
 )
 
+// Nosurf adds CSRF protection to all POST requests.
 func NoSurf(next http.Handler) http.Handler {
+	// change this to true when in production
+	app.InProduction = false
+
 	csrfHandler := nosurf.New(next)
 
 	csrfHandler.SetBaseCookie(http.Cookie{
 		HttpOnly: true,
 		Path:     "/",
-		Secure:   false,
+		Secure:   app.InProduction,
 		SameSite: http.SameSiteLaxMode,
 	})
 	return csrfHandler
+}
+
+// SessionLoads loads and saves the session on every request
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
 }
