16acbe4547597a3f3a678199cd4ce7bc20efb3ba
Reuben deVries <reubendevries@gmail.com>
2023-11-30 16:08:00 -0800

    Created base for new project.
    Signed-off-by: Reuben deVries <reubendevries@gmail.com>

---
+++ Section-05/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"golang_training/Section-04/01-SessionsPackage/config"
+	"golang_training/Section-04/01-SessionsPackage/handlers"
+	"golang_training/Section-04/01-SessionsPackage/render"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main application function.
+func main() {
+	// when in Production set to true.
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache.")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+
+	fmt.Printf("Starting application on port %s", portNumber)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
